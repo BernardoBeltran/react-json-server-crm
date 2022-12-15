dee9ea3b7d6458b1146c7b41dc760c99f030ec72
Bernardo <ber100023@gmail.com>
2022-12-15 14:10:33 -0500

    Color de fondo del aside actualizado

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,7 +6,7 @@ const Layout = () => {
 
     return (
         <div className="md:flex md:min-h-screen">
-            <aside className="md:w-1/4 bg-blue-900 px-5 py-10">
+            <aside className="md:w-1/4 bg-violet-900 px-5 py-10">
                 <h2 className="text-4xl font-black text-center text-white">
                     CRM - Clientes
                 </h2>
